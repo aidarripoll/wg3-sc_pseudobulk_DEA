c9a96671213cab8ab84fd4830664ebda2bc0cab1
aidarripoll <35876619+aidarripoll@users.noreply.github.com>
2025-06-03 16:03:15 +0200

    Pseudobulk DEA using log2cpmINRT and LMER (main)
    Used in Aquino 2024 and in our preprint Sopena Rios and Ripoll Cladellas 2025

---
+++ pseudobulkDEA_inrt_lmer.R
@@ -0,0 +1,403 @@
+#!/usr/bin/env Rscript
+
+# options parser
+shhh <- suppressPackageStartupMessages
+shhh(library(optparse))
+option_list = list(
+  make_option(c("-l", "--cell_level"), action="store", default="L1", type='character',
+              help="Cell type levels: Low resolution (L1) or high resolution (L2)"),
+  make_option(c("-c", "--cell_type"), action="store", default=NULL, type='character',
+              help="Cell types in the low cell level resolution (l1) or in the high cell level resolution (l2)"),
+  make_option(c("--phenotype"), action="store", default=NULL, type='character',
+              help="SEX/age"),
+  make_option(c("--nCells_per_donor"), action="store", default=FALSE, type='logical',
+              help="TRUE or FALSE"),
+  make_option(c("--interaction"), action="store", default=NULL,  type='character',
+              help="SEX.age"),
+  make_option(c("--covariates"), action="store", default='pseudobulkDEA_dreamlet.covariates.tab', type='character',
+              help="Covariates file."),
+  make_option(c("--min_prop"), action="store", default=0.2, type='double',
+              help="In dreamlet::processAssays(), minimum proportion of retained samples with non-zero counts for a gene to be retained."),
+  make_option(c("--vp_reduced"), action="store", default=FALSE, type='logical',
+              help="Not estimate the effect of the batch factor in the VariancePartition analysis."),
+  make_option(c("--combined_data"), action="store", default=FALSE, type='logical',
+              help="If the data from more than one study has been previously merged in one object."),
+  make_option(c("--genoPC1"), action="store", default=FALSE, type='logical',
+              help="If the data contains more than one genetic ancestry."),
+  make_option(c("--filter_replicates"), action="store", default=TRUE, type='logical',
+              help="If the data contains more than one sample per donor."),
+  make_option(c("-i", "--in_dir"), action="store", default='input', type='character',
+              help="Input directory: WG2 output directory."),
+  make_option(c("-o","--out_dir"), action="store", default="pseudobulkDEA_dreamlet", type='character',
+              help="Output directory.")
+)
+opt = parse_args(OptionParser(option_list=option_list))
+
+################################# Set Variables ################################
+ # Set working directory
+cwd <- getwd()
+setwd(cwd)
+
+# Loading functions
+functions.fn <- 'scripts/pseudobulkDEA_inrt_lmer_functions.R'
+print(paste0('Loading functions from: ', functions.fn))
+source(functions.fn)
+
+common_functions.fn <- 'scripts/common_functions.R'
+print(paste0('Loading common functions from: ', common_functions.fn))
+source(common_functions.fn)
+
+# Testing
+## wijst2018
+# opt$cell_type <- 'B'
+# opt$phenotype <- 'SEX'
+# opt$nCells_per_donor <- TRUE
+# opt$in_dir <- '/gpfs/projects/bsc83/Projects/scRNAseq/aripol1/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_wijst2018/input'
+# opt$out_dir <- '/gpfs/projects/bsc83/Projects/scRNAseq/aripol1/sc-eqtlgen-consortium-pipeline/ongoing/wg3-sc_pseudobulk_DEA-updated/wg3_wijst2018/pseudobulkDEA_inrt_lmer'
+
+## okada
+# opt$cell_type <- 'B'
+# opt$in_dir <- '/gpfs/projects/bsc83/Projects/scRNAseq/aripol1/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_okada/input'
+# opt$covariates <- 'pseudobulkDEA_dreamlet.covariates.okada.tab'
+# opt$out_dir <- 'pseudobulkDEA_inrt_lmer.okada'
+# opt$covariates <- 'pseudobulkDEA_dreamlet.covariates.test.tab'
+# opt$out_dir <- 'pseudobulkDEA_inrt_lmer.okada.test'
+
+## Li
+# opt$cell_type <- 'B'
+# opt$in_dir <- '/gpfs/projects/bsc83/Projects/scRNAseq/aripol1/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_Li.addMetadata/input'
+# opt$out_dir <- 'pseudobulkDEA_inrt_lmer.Li'
+# opt$covariates <- 'pseudobulkDEA_dreamlet.covariates.Li.tab'
+
+## Nawijn_samplecondition
+# opt$cell_type <- 'B'
+# opt$in_dir <- '/gpfs/projects/bsc83/Projects/scRNAseq/aripol1/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_Nawijn.addMetadata.sample_condition/input'
+# opt$out_dir <- 'pseudobulkDEA_inrt_lmer.Nawijn_samplecondition'
+
+## Ye
+# opt$cell_type <- 'B'
+# opt$in_dir <- '/gpfs/projects/bsc83/Projects/scRNAseq/aripol1/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_Ye.addMetadata/input'
+# opt$out_dir <- 'pseudobulkDEA_inrt_lmer.Ye'
+# opt$genoPC1 <- TRUE
+
+# Input/Output directories
+in.dir <- paste0(opt$in_dir, '/', opt$cell_level, '/')
+out.dir <- paste0(opt$out_dir, '/', opt$cell_level, '/', opt$cell_type, '/')
+if(opt$vp_reduced){out.dir <- paste0(out.dir, 'vp_reduced/')}
+if(opt$nCells_per_donor){out.dir <- paste0(out.dir, 'nCells_per_donor/')}
+out.dir <- paste0(out.dir, '/', opt$interaction, '/')
+if(!dir.exists(out.dir)){dir.create(out.dir, recursive = T)}
+
+# Covariates 
+covs.fn <- opt$covariates
+
+# Seurat object
+so.fn <- paste0(in.dir, opt$cell_type, '.Qced.Normalized.SCs.Rds')
+
+# smf file
+smf.fn <- paste0(opt$in_dir, '/smf.txt')
+
+# qtlInput Pcs file
+qtlInput_Pcs.fn <- paste0(in.dir, opt$cell_type, '.qtlInput.Pcs.txt')
+
+# Print report
+print(paste0('Cell level: ', opt$cell_level))
+print(paste0('Cell type: ', opt$cell_type))
+print(paste0('Phenotype: ', opt$phenotype))
+print(paste0('nCells per donor: ', opt$nCells_per_donor))
+print(paste0('Interaction: ', opt$interaction))
+print(paste0('Covariates file: ', opt$covariates))
+print(paste0('Min prop: ', opt$min_prop))
+print(paste0('VariancePartition reduced: ', opt$vp_reduced))
+print(paste0('Combined data: ', opt$combined_data))
+print(paste0('genoPC1: ', opt$genoPC1))
+print(paste0('Filter replicates: ', opt$filter_replicates))
+print(paste0('Input directory: ', opt$in_dir))
+print(paste0('Input file: ', so.fn))
+print(paste0('Output directory: ', out.dir))
+cat('\n')
+
+################################ Read input data ###############################
+# Read covariates file
+print(paste0('Reading covariates file in: ',covs.fn))
+covs.df <- read.table(covs.fn, header = T)
+covariates <- covs.df$covariate
+phenotypes <- covs.df[covs.df$type=='fixed',]$covariate
+
+# Read Seurat object file
+print(paste0('Reading PBMC seurat object file in: ',so.fn))
+system.time(pbmc_i <- readRDS(so.fn))
+DefaultAssay(pbmc_i) <- "RNA"
+
+# Testing
+# pbmc_raw <- pbmc_i
+# set.seed(123)
+# cells <- sample(colnames(pbmc_i), 5000)
+# pbmc_i <- pbmc_i[,cells]
+
+########################## Pre-processing (sc/pseudobulk) ###########################
+# Modify metadata
+print('Modifying metadata...')
+pbmc <- modify_metadata_sc(pbmc_i, covariates, covs.df)
+
+# Filter donors (Donor_Pool) based on genotypes and QC filtering
+print('Filter donors (Donor_Pool) based on genotypes and QC filtering...')
+pbmc <- filter_donors(pbmc, smf.fn, qtlInput_Pcs.fn)
+
+# (optional) Filter replicates: picking only one sample (Donor_Pool) per donor (Donor)
+if(opt$filter_replicates){
+  cell.md <- pbmc@meta.data
+  donor.md <- unique(cell.md[,c('Donor', 'Donor_Pool')])
+  if(any(table(donor.md$Donor)>1)){
+    print('Filter replicates: picking only one sample (Donor_Pool) per donor (Donor)...')
+    # Check
+    donors <- unique(donor.md$Donor)
+    ndonors <- length(donors)
+    samples <- unique(donor.md$Donor_Pool)
+    nsamples <- length(samples)
+    donor_replicates.vec <- table(donor.md$Donor)[table(donor.md$Donor)>1]
+    donor_replicates <- names(donor_replicates.vec)
+    donor_replicates.n <- length(donor_replicates)
+    print(paste0('# of initial Donor: ', ndonors))
+    print(paste0('# of initial Donor_Pool: ', nsamples))
+    print(paste0('# of initial Donor with replicates: ', donor_replicates.n))
+    
+    # Filtering samples
+    samples_kept.list <- sapply(donors, function(i) filter_replicates(i, cell.md), simplify = FALSE)
+    samples_kept <- unname(unlist(samples_kept.list))
+    print(paste0('# of filtered Donor_Pool: ', length(samples_kept)))
+    cell_md.filtered <- droplevels(cell.md[cell.md$Donor_Pool%in%samples_kept,])
+    ncells.filtered <- nrow(cell_md.filtered)
+    ndonors.filtered <- length(unique(cell_md.filtered$Donor))
+    print(paste0('# of filtered Donor: ', ndonors.filtered, ' (nCells = ', ncells.filtered, ')'))
+    cells_kept <- rownames(cell_md.filtered)
+    
+    # Filtering seurat object
+    pbmc <- pbmc[,cells_kept]
+    pbmc@meta.data <- cell_md.filtered
+  }
+}
+
+# (optional) Add a new variable for the previously merged studies
+if(opt$combined_data){
+  # Add a new variable in the seurat object
+  pbmc$Dataset <- str_split_fixed(rownames(pbmc@meta.data), '_', 2)[,1]
+  pbmc$Dataset <- as.factor(pbmc$Dataset)
+  nDataset <- length(unique(pbmc$Dataset))
+  print('The previous datasets with nCells were merged:')
+  print(table(pbmc$Dataset))
+  
+  # Add a new covariate in the covariates file
+  Dataset.df <- data.frame(covariate = 'Dataset',
+                           type = 'random',
+                           class = 'factor')
+  covs.df <- rbind(covs.df, Dataset.df)
+}
+
+# (optional) Add a new variable for genoPC1
+if(opt$genoPC1){
+  print('The data contains more than one genetic ancestry...')
+  
+  # Read qtlInput_Pcs
+  qtlInput_Pcs <- read.delim(qtlInput_Pcs.fn, check.names = FALSE)
+  rownames(qtlInput_Pcs) <- qtlInput_Pcs[,1]
+  qtlInput_Pcs <- qtlInput_Pcs[,-1]
+  
+  # Select only the filtered Donor_Pool samples
+  Donor_Pool.vec <- unique(pbmc$Donor_Pool)
+  qtlInput_Pcs.filt <- qtlInput_Pcs[rownames(qtlInput_Pcs)%in%Donor_Pool.vec,]
+  qtlInput_Pcs.filt.df <- data.frame(Donor_Pool = rownames(qtlInput_Pcs.filt),
+                                     genoPC1 = qtlInput_Pcs.filt$genoPC1)
+  
+  # Add a new variable in the seurat object
+  pbmc@meta.data <- merge(pbmc@meta.data, qtlInput_Pcs.filt.df, by = 'Donor_Pool')
+  rownames(pbmc@meta.data) <- pbmc@meta.data$Barcode
+  
+  # Add a new covariate in the covariates file
+  genoPC1.df <- data.frame(covariate = 'genoPC1',
+                           type = 'fixed',
+                           class = 'integer')
+  covs.df <- rbind(covs.df, genoPC1.df)
+}
+
+# Check covariates
+print('Checking covariates...')
+cov_vars <- covs.df$covariate
+metadata_vars <- colnames(pbmc@meta.data)
+if(!all(cov_vars%in%metadata_vars)){
+  err_message <- paste0('Your metadata variables in: ', opt$covariates, ' should be in the seurat object metadata: ', so.fn)
+  stop(err_message)
+}
+
+################################ Aggregate to pseudobulk ################################
+# Get contrasts according to the phenotypes
+contrast_coefName.list <- sapply(phenotypes, function(i) get_coefName(i, pbmc), simplify = FALSE)
+contrast_coefName.list <- Filter(Negate(is.null), contrast_coefName.list)
+                                 
+# Convert Seurat object to SCE object
+print('Convert Seurat to SCE object...')
+sce <- as.SingleCellExperiment(pbmc)
+# sce_fn <- paste0(out.dir, 'sce.rds')
+# print(paste0('Saving SCE object: ', sce_fn))
+# saveRDS(sce, sce_fn)
+cat('\n')
+
+# Aggregate to pseudobulk: Dreamlet, like muscat, performs analysis at the pseudobulk-level by summing raw counts across cells for a given sample and cell type. 
+# aggregateToPseudoBulk is substantially faster for large on-disk datasets than muscat::aggregateData.
+print('Performing pseudobulk...')
+colData(sce)$cell_type <- opt$cell_type
+colData(sce)$cell_type <- as.factor(colData(sce)$cell_type)
+colData(sce)$Donor_Pool <- as.factor(colData(sce)$Donor_Pool)
+system.time(pb <- aggregateToPseudoBulk(sce,
+                                        assay = "counts",     
+                                        cluster_id = "cell_type", 
+                                        sample_id = "Donor_Pool",
+                                        verbose = FALSE))
+pb_fn <- paste0(out.dir, 'aggregateToPseudoBulk.rds')
+print(paste0('Saving aggregateToPseudoBulk results: ', pb_fn))
+saveRDS(pb, pb_fn)
+pb_raw <- pb
+cat('\n')
+
+################################ Filtering & Normalization ################################
+# Filtering: processAssays (genes & samples)
+system.time(process_data.list <- process_data(ge_dge = pb, 
+                                              covariates = covs.df, 
+                                              min_prop = opt$min_prop, 
+                                              cell_counts = opt$nCells_per_donor,
+                                              vp_reduced = opt$vp_reduced,
+                                              out_dir = out.dir, 
+                                              gene_test = c('VP','DEA')))
+                                              
+################################ Testing ################################
+# Variables
+genes_expressed <- rownames(process_data.list$expr_counts)
+norm_methods <- c('voomWithDreamWeights', 'log2cpm')
+
+# Apply function (parallel)
+## testing
+# genes_expressed.all <- genes_expressed #testing
+# set.seed(123) #testing
+# genes_expressed <- sample(genes_expressed.all, 50) #testing
+# system.time(lmer_out <- sapply(norm_methods, function(i)
+#   sapply(genes_expressed, function(j) lmer_func(g = j, 
+#                                                 norm = i,
+#                                                 process_data_list = process_data.list, 
+#                                                 phenotype = opt$phenotype), simplify = FALSE), simplify = FALSE)) #to debug
+
+print('Fitting lmer...')  
+system.time(lmer_out <- setNames(
+    mclapply(norm_methods, function(i){
+      setNames(
+        mclapply(genes_expressed, function(j) lmer_func(g = j, 
+                                                        norm = i, 
+                                                        process_data_list = process_data.list, 
+                                                        phenotype = opt$phenotype)), 
+        genes_expressed  # Set names for inner mclapply output
+      )
+    }),
+    norm_methods  # Set names for outer mclapply output
+  )
+) #faster
+
+# Rearrange the data
+items <- c('model', 'nearZeroVar')
+lmer_out.items <- sapply(items, function(i) lapply(lmer_out, function(metric) 
+  lapply(metric, function(x) x[[i]])), simplify = FALSE)
+
+## nearZeroVar report
+nearZeroVar.list <- lmer_out.items$nearZeroVar
+nearZeroVar.bymetric.out <- lapply(nearZeroVar.list, function(x){
+  df <- do.call("rbind", x)
+  df$ID <- rownames(df)
+  return(df)
+})
+
+# Save output
+## Phenotype term
+phenotype_term <- opt$phenotype
+if(opt$phenotype%in%c('SEX','age_cat', 'age_cat_all')){
+  Group_order <- c('M','F')
+  if(opt$phenotype%in%c('age_cat', 'age_cat_all')){
+    Group_order <- c('Y','O')
+  }
+  phenotype_term <- paste0(phenotype_term, Group_order[2])
+}
+
+## LM output
+lmer.by_metric <- lmer_out.items$model
+lmer.by_metric.out <- lapply(lmer.by_metric, function(x) do.call("rbind", x))
+if(!is.null(opt$interaction)){phenotype_term <- grep(':', unique(lmer.by_metric.out[[1]]$term), value = TRUE)}
+lmer.by_metric.out <- lapply(lmer.by_metric.out, function(x) x[x$term==phenotype_term,])
+lmer.by_metric.out <- lapply(lmer.by_metric.out, function(x){
+  rownames(x) <- sub("\\.[^.]+$", "", rownames(x))
+  x$ID <- rownames(x)
+  return(x)
+})
+lmer.by_metric.fn <- paste0(out.dir, opt$phenotype, '.lmer.by_metric.rds')
+print(paste0('Saving output (tidy lmer) in: ', lmer.by_metric.fn))
+saveRDS(lmer.by_metric.out, lmer.by_metric.fn)
+
+## nearZeroVar output
+nearZeroVar.bymetric.fn <- paste0(out.dir, opt$phenotype, '.nearZeroVar.by_metric.rds')
+print(paste0('Saving output (nearZeroVar report) in: ', nearZeroVar.bymetric.fn))
+saveRDS(nearZeroVar.bymetric.out, nearZeroVar.bymetric.fn)
+
+# Report failed genes
+check_list.norm_methods <- lapply(lmer.by_metric.out, function(x){xx <- is.na(unique(x[['estimate']])); names(xx) <- rownames(x); return(xx)})
+check_failed <- function(i){
+  check_list <- check_list.norm_methods[[i]]
+  missing_cases <- length(check_list[check_list=='TRUE'])
+  missing_genes <- names(check_list[check_list=='TRUE'])
+  total_cases <- length(check_list)
+  complete_cases <- total_cases - missing_cases
+  print(paste0('There is no variability (', i, ') in: ', missing_cases, ' DEGs...'))
+}
+check_var <- lapply(names(check_list.norm_methods), function(i) check_failed(i))
+
+# Arrange data for assigning the mode (x = voomWithDreamWeights and y = log2cpm)
+print('Arranging data...')
+## add nearZeroVar info
+voom_df <- merge(lmer.by_metric.out$voomWithDreamWeights, nearZeroVar.bymetric.out$voomWithDreamWeights, by = 'ID')
+inrt_df <- merge(lmer.by_metric.out$log2cpm, nearZeroVar.bymetric.out$log2cpm, by = 'ID')
+lmer.by_gene.df <- merge(voom_df, inrt_df, by = c("effect", "term", "ID"), suffixes = c('.voomWithDreamWeights', '.log2cpm'))
+
+## calculate FDR and split by gene again
+lmer.by_gene.df$fdr.voomWithDreamWeights <- p.adjust(lmer.by_gene.df$p.value.voomWithDreamWeights, 'fdr')
+lmer.by_gene.df$fdr.log2cpm <- p.adjust(lmer.by_gene.df$p.value.log2cpm, 'fdr')
+lmer.by_gene <- split(lmer.by_gene.df, lmer.by_gene.df$ID)
+
+## save
+lmer_nearZeroVar.by_metric.fn <- paste0(out.dir, opt$phenotype, '.lmer_nearZeroVar.by_metric.rds')
+print(paste0('Saving joined output (tidy lmer + nearZeroVar report) in: ', lmer_nearZeroVar.by_metric.fn))
+saveRDS(lmer.by_gene.df, lmer_nearZeroVar.by_metric.fn)
+
+## check
+print('FDR < 0.05...')
+table(lmer.by_gene.df$fdr.voomWithDreamWeights<0.05, lmer.by_gene.df$estimate.voomWithDreamWeights>0) #voomWithDreamWeights
+table(lmer.by_gene.df$fdr.log2cpm<0.05, lmer.by_gene.df$estimate.log2cpm>0) #log2cpm
+
+cat('\n')
+
+print('FDR < 0.1...')
+table(lmer.by_gene.df$fdr.voomWithDreamWeights<0.1, lmer.by_gene.df$estimate.voomWithDreamWeights>0) #voomWithDreamWeights
+table(lmer.by_gene.df$fdr.log2cpm<0.1, lmer.by_gene.df$estimate.log2cpm>0) #log2cpm
+
+cat('\n')
+
+print('p-nominal < 0.01...')
+table(lmer.by_gene.df$p.value.voomWithDreamWeights<0.01, lmer.by_gene.df$estimate.voomWithDreamWeights>0) #voomWithDreamWeights
+table(lmer.by_gene.df$p.value.log2cpm<0.01, lmer.by_gene.df$estimate.log2cpm>0) #log2cpm
+
+cat('\n')
+
+print('p-nominal < 0.05...')
+table(lmer.by_gene.df$p.value.voomWithDreamWeights<0.05, lmer.by_gene.df$estimate.voomWithDreamWeights>0) #voomWithDreamWeights
+table(lmer.by_gene.df$p.value.log2cpm<0.05, lmer.by_gene.df$estimate.log2cpm>0) #log2cpm
+
+cat('\n')
+
+print('p-nominal < 0.1...')
+table(lmer.by_gene.df$p.value.voomWithDreamWeights<0.1, lmer.by_gene.df$estimate.voomWithDreamWeights>0) #voomWithDreamWeights
+table(lmer.by_gene.df$p.value.log2cpm<0.1, lmer.by_gene.df$estimate.log2cpm>0) #log2cpm
+
